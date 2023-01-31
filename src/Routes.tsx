c97456b6d5b26dd0940a497168eb1a78e8f8b15a
Kalel Poeta <kalel.poeta@orbisag.ch>
2023-01-31 14:49:06 +0100

    Completed course added 404 page

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -3,6 +3,7 @@ import { BlogDetails } from "./components/BlogDetails";
 import { Create } from "./components/Create";
 import { Home } from "./components/Home";
 import { Navbar } from "./components/Navbar";
+import { NotFound } from "./components/NotFound";
 
 export function AppRoutes() {
   return (
@@ -14,6 +15,7 @@ export function AppRoutes() {
             <Route path="/" element={<Home />} />
             <Route path="/create" element={<Create />} />
             <Route path="/blogs/:id" element={<BlogDetails />} />
+            <Route path="*" element={<NotFound />} />
           </Routes>
         </div>
       </div>
